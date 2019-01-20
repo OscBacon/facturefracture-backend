e7779df8701b4f8357212f23ab4627021fe0d3ac
Oscar Baracos <oscar.baracos@gmail.com>
2019-01-19 22:46:06 -0500

    Fixed code

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template, \
-    send_from_directory
-from werkzeug.utils import secure_filename
-from datetime import datetime
+from flask import Flask, flash, request, redirect, render_template
 import string
 import random
-from manage_blobs import upload_blob
-import base64
+import json
 
-UPLOAD_FOLDER = os.path.join('static','uploads')
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'my dude'
 
-# Currently, bills can be uploaded on a webpage
+
 @app.route("/create_bill", methods=["GET", "POST"])
 def create_bill():
     if request.method == "POST":
@@ -29,20 +25,13 @@ def create_bill():
 
         if file:
             code = generate_code()
-            timestamp = datetime.now().strftime('%d%m%y%H%M%S')
-            filename = code + '_' + timestamp + '.jpg'
+            filename = code + '.jpg'
 
-            try:
-                filepath = os.path.join(os.sep, "/bills-images", filename)
-                file.save(filepath)
-                azure_filepath="https://facturefracture.blob.core.windows.net/bills-images/" + \
-                    filename
-                return render_template("uploaded_file.html",
-                                       filepath=azure_filepath, code=code)
-
-            except:
-                flash("Upload Error!")
-                return redirect(request.url)
+            filepath = os.path.join(os.sep, "bills-images", filename)
+            file.save(filepath)
+            azure_filepath = "https://facturefracture.blob.core.windows.net/bills-images/" + \
+                filename
+            return render_template("uploaded_file.html", filepath=azure_filepath, code=code)
 
     return render_template("upload_file.html")
 
@@ -50,9 +39,10 @@ def create_bill():
 def uploaded_file(filename):
     return os.path.join(UPLOAD_FOLDER, filename)
 
+
 def generate_code():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) \
-                   for _ in range(6))
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
 
 def create_json(code, total, user):
     bill_dict = {
