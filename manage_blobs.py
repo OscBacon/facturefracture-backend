f08d4b4f24146411cb270fa740a7aaca58e8103d
Oscar Baracos <oscar.baracos@gmail.com>
2019-01-20 05:32:33 -0500

    Delete manage_blobs.py

--- manage_blobs.py
+++
@@ -1,8 +0,0 @@
-from azure.storage.blob import BlockBlobService
-
-def upload_blob(filename, filepath):
-    block_blob_service = BlockBlobService(
-        account_name='facturefracture',
-        account_key='53komISgN0kGVHUKNj9e55u9h9avf09dyu/unBadeROztPv9rak/UvWwmCejmgm9DnWXJtLUU9THU08pIihzIw==')
-
-    block_blob_service.create_blob_from_path('bills-images', filename, filepath)
